ddfef7c3614a6b8aa8cc73a6aff3acfdc16f506a
GhayyasT <gayyastariq96@gmail.com>
2021-01-31 15:44:42 +0500

    FirstCommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace SmartImageForm_v1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.logoPictureBox = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.lblItemName = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.lblItemNumber = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.btnImage1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// logoPictureBox
+			// 
+			this.logoPictureBox.Location = new System.Drawing.Point(1, 1);
+			this.logoPictureBox.Name = "logoPictureBox";
+			this.logoPictureBox.Size = new System.Drawing.Size(60, 60);
+			this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.logoPictureBox.TabIndex = 1;
+			this.logoPictureBox.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(175, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(223, 25);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Smart Image Manager";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.panel1.Controls.Add(this.lblItemName);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.lblItemNumber);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Location = new System.Drawing.Point(1, 67);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(603, 35);
+			this.panel1.TabIndex = 3;
+			// 
+			// lblItemName
+			// 
+			this.lblItemName.AutoSize = true;
+			this.lblItemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblItemName.Location = new System.Drawing.Point(335, 8);
+			this.lblItemName.Name = "lblItemName";
+			this.lblItemName.Size = new System.Drawing.Size(89, 18);
+			this.lblItemName.TabIndex = 6;
+			this.lblItemName.Text = "ItemNumber";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(240, 8);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(89, 18);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "ItemName:";
+			// 
+			// lblItemNumber
+			// 
+			this.lblItemNumber.AutoSize = true;
+			this.lblItemNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblItemNumber.Location = new System.Drawing.Point(108, 8);
+			this.lblItemNumber.Name = "lblItemNumber";
+			this.lblItemNumber.Size = new System.Drawing.Size(89, 18);
+			this.lblItemNumber.TabIndex = 4;
+			this.lblItemNumber.Text = "ItemNumber";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(3, 8);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(99, 18);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "ItemNumber";
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+			this.tableLayoutPanel1.Controls.Add(this.btnImage1, 0, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 108);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(603, 377);
+			this.tableLayoutPanel1.TabIndex = 4;
+			// 
+			// btnImage1
+			// 
+			this.btnImage1.Location = new System.Drawing.Point(3, 3);
+			this.btnImage1.Name = "btnImage1";
+			this.btnImage1.Size = new System.Drawing.Size(194, 119);
+			this.btnImage1.TabIndex = 0;
+			this.btnImage1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(605, 486);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.logoPictureBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Smart Image Form";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox logoPictureBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label lblItemName;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label lblItemNumber;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button btnImage1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using FMdotNet__DataAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartImageForm_v1
+{
+	public partial class Form1 : Form
+	{
+		private readonly string relativePath = string.Empty;
+		private readonly string projectRecordId = "1404";
+		private readonly string filePath = @"C:\Users\user\Desktop\Smart_Image_Form_Data.txt";
+		private readonly string server = "prod01.thesmartdesigner.com";
+		private readonly string database = "V6_Data_99999";
+		private readonly string userName = "tss";
+		private readonly string password = "2504B";
+		private readonly string itemLayout = "|Specs";
+		private readonly string productLayout = "|Products";
+		private readonly string graphicLayout = "|Graphics";
+		private FMS18 fmServer;
+		private string token = string.Empty;
+
+		public Form1()
+		{
+			InitializeComponent();
+			fmServer = new FMS18(server, userName, password);
+			relativePath = new Uri(Directory.GetCurrentDirectory()).LocalPath.Replace("bin", "").Replace("Debug", "").Replace(@"\\", @"\");
+		}
+
+		private async void Form1_Load(object sender, EventArgs e)
+		{
+			logoPictureBox.Image = Image.FromFile($@"{relativePath}Images\logo.jpg");
+			var lines = File.ReadLines(filePath).ToList();
+			await AuthenticateFileMakerServer();
+			await PopulateData(173956, 91086);
+		}
+
+		private async Task PopulateData(int itemRecordId, int porductRecordId)
+		{
+			fmServer.SetLayout(productLayout);
+			var getProductRequest = fmServer.FindRequest(91806);
+			var getProductResponse = await getProductRequest.Execute();
+
+			FMRecord findResultRow = getProductResponse.data.foundSet.records.First();
+			FMRecordSet graphicsPortalData = null;
+
+			foreach (var relatedData in findResultRow.relatedRecordSets)
+			{
+				if (!string.IsNullOrEmpty(relatedData.tableLayoutObjectName) && relatedData.tableLayoutObjectName.ToLower() == "graphicsportal")
+				{
+					graphicsPortalData = relatedData;
+					break;
+				}
+			}
+
+			for (int i = 0; i < 9; i++)
+			{
+				var pictureBox = GetCurrentPictureBox(i);
+
+				if (i > 10 && i < graphicsPortalData.records.Count())
+				{
+					var currentRow = graphicsPortalData.records.ElementAt(i);
+					var file = currentRow.fieldsAndData["File"];
+
+				}
+				else
+				{
+					pictureBox.Image = ResizedSelectImage();
+				}
+			}
+
+			//MessageBox.Show($"Error while retrieving data, {fmServer.lastErrorMessage}");
+		}
+
+		private async Task AuthenticateFileMakerServer()
+		{
+			try
+			{
+				fmServer.SetFile(database);
+				token = await fmServer.Authenticate();
+			}
+			catch (Exception ex)
+			{
+				if (fmServer.lastErrorCode > 0)
+					MessageBox.Show($"FileMaker error code, message: {fmServer.lastErrorCode}, {fmServer.lastErrorMessage}");
+				else
+					MessageBox.Show(ex.Message);
+			}
+		}
+
+		private Image ResizedSelectImage()
+		{
+			var imageSize = new Size(194, 119);
+			return new Bitmap(Image.FromFile($@"{relativePath}Images\select.png"), imageSize);
+		}
+
+		private Image Convertbase64ToImage(string base64String)
+		{
+			Image image;
+			var bytes = Convert.FromBase64String(base64String);
+			using (MemoryStream ms = new MemoryStream(bytes))
+			{
+				image = Image.FromStream(ms);
+			}
+
+			return image;
+		}
+
+		private Button GetCurrentPictureBox(int index)
+		{
+			if (index == 0)
+				return btnImage1;
+			else if (index == 1)
+				return btnImage1;
+			else if (index == 2)
+				return btnImage1;
+			else if (index == 3)
+				return btnImage1;
+			else if (index == 4)
+				return btnImage1;
+			else if (index == 5)
+				return btnImage1;
+			else if (index == 6)
+				return btnImage1;
+			else if (index == 7)
+				return btnImage1;
+			else
+				return btnImage1;
+		}
+	}
+}
